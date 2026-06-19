150885659b568d14d1d7b1a0192db40b62c7dbc6
iaimpiyush <ithastobepiyush@gmail.com>
2026-06-19 13:35:17 +0530

    palindrome solution updated

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -1,6 +1,22 @@
+var isPalindrome = (n) => {
+    let copy = n
+    let reversed = 0
+    if (n<0) return false
+    
+    while(n>0){
+        let remainder = n % 10
+        reversed = (10*reversed) + remainder
+        n = Math.floor(n/10)
+    }
+    return reversed === copy 
+}
+console.log(999, isPalindrome(999))
+console.log(-121, isPalindrome(-121))
+
+
 
 // is palindrome without converting it to the string
-var isPalindrome = (inputNumber) => {
+var isPalindrome1 = (inputNumber) => {
     if(inputNumber<0) return false
     if(inputNumber<10) return true
     
@@ -19,15 +35,17 @@ var isPalindrome = (inputNumber) => {
     
     return inputNumber === reversedNumber
 }
-console.log(100000000000, isPalindrome(100000000000))
-console.log(-121, isPalindrome(-121))
+// console.log(100000000000, isPalindrome1(100000000000))
+// console.log(-121, isPalindrome1(-121))
 
 
 
 
 // palindrome for the string
-var isPalindrome1 = (x) => {
+var isPalindrome2 = (x) => {
     // validation
     const reverseString = x.toString().split("").reverse().join("")
     return reverseString === x.toString()
 }
+// console.log(100000000000, isPalindrome2(100000000000))
+// console.log(-121, isPalindrome2(-121))
